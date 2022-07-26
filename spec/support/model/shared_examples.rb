b3f17fed20b4de11fe437f41b1aca87b7005aa74
nigussolomon <nigussolomon444@gmail.com>
2022-07-26 20:10:18 +0300

    created the shared model examples - see TB-0/0.1

---
+++ spec/support/model/shared_examples.rb
@@ -0,0 +1,29 @@
+VALIDATIONS = {
+    presence: 'validate_presence_of',
+    uniqueness: 'validate_uniqueness_of',
+    numericality: 'validate_numericality_of',
+    belong_to: 'belong_to',
+    have_many: 'have_many',
+    have_and_belong_to_many: 'have_and_belong_to_many' 
+}.freeze
+
+
+Rspec.shared_examples 'model_shared_spec' do |factory, attribs, subj = false|
+    subject { create(:factory) } unless subj
+
+    it 'has a valid factory' do
+        expect(create(factory)).to be_valid
+    end
+
+    attribs.each do |attr|
+        attr.each do |k,v|
+            if v.instance_of?(Array)
+                v.each do |validation|
+                    it { is_expected.to(send(VALIDATIONS[validation], k))}
+                end
+            else
+                it { is_expected.to(send(VALIDATIONS[v], k))}
+            end
+        end  
+    end
+end
